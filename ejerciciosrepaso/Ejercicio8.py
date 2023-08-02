f49c0318f174a65f924dc07fa761c920d55bbb08
Mateovarela04 <mateo.varela54@gmail.com>
2023-08-01 23:30:39 -0500

    En este commit se guarda la solución del ejercicio #8

---
+++ ejerciciosrepaso/Ejercicio8.py
@@ -0,0 +1,10 @@
+def invertir_lista(lista):
+    lista.reverse()
+    return lista
+
+lista = [4, 67, 78, 3, 0]
+
+#Se llama la función
+lista_invertida = invertir_lista(lista)
+
+print("Lista invertida es:", lista_invertida)

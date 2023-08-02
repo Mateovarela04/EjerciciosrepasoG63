63eb87461c2cfb440c3bdb407be39452b33fcdea
Mateovarela04 <mateo.varela54@gmail.com>
2023-08-01 23:22:53 -0500

    En este commit se guarda la solución del ejercicio #7

---
+++ ejerciciosrepaso/Ejercicio7.py
@@ -0,0 +1,13 @@
+def encontrar_mayor_y_menor(lista):
+    numero_mas_grande = max(lista)
+    numero_mas_pequeno = min(lista)
+    return numero_mas_grande, numero_mas_pequeno
+
+# Lista de ejemplo
+lista_numeros = [5, 2, 9, 1, 7, 3]
+
+# Llamamos a la función
+mas_grande,mas_pequeno = encontrar_mayor_y_menor(lista_numeros)
+
+print("El número más grande es:", mas_grande)
+print("El número más pequeño es:", mas_pequeno)

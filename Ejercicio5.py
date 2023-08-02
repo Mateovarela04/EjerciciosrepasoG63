44d3bfef1f621be3f0691f990311e9c832e41317
Mateovarela04 <mateo.varela54@gmail.com>
2023-08-01 23:11:14 -0500

    En este commit se guarda la solución del ejercicio #5

---
+++ Ejercicio5.py
@@ -0,0 +1,5 @@
+#Crear una función que convierta grados Fahrenheit a grados Celsius.
+def fahrenheit_a_celsius(grados_fahrenheit):
+
+    grados_celsius = (grados_fahrenheit - 32) * 5/9
+    return grados_celsius

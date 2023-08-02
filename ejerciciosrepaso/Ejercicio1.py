abca69020a4d04c2f767edec9e8ae1ac72704b80
Mateovarela04 <mateo.varela54@gmail.com>
2023-08-01 22:17:03 -0500

    En este commit se guarda la solución del ejercicio #1

---
+++ ejerciciosrepaso/Ejercicio1.py
@@ -0,0 +1,8 @@
+"""Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla."""
+
+nombre= str(input("Ingresa tu nombre: "))
+
+edad= int(input("Ingresa tu edad: "))
+
+print("Tu nombre es", nombre, "Tienes",edad, "años." )
+
